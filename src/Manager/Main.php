3d58ec668cb083bf31c04a798c9031c890009919
jasonwynn10 <jasonwynn10@gmail.com>
2016-04-17 17:34:34 -0400

    Update Main.php

--- src/Manager/Main.php
+++ src/Manager/Main.php
@@ -6,77 +6,38 @@
 | |__| (_) \__ \ |_ | |  __/ (_| | | | | | |
 |_____\___/|___/\__||_|\___|\__,_|_| |_| |_|
  */
-
 namespace Manager;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as Color;
-use pocketmine\event\Listener;
-use pocketmine\Server;
-use pocketmine\plugin\PluginLoadOrder;
-
-class Main extends pluginBase implements Listener  {
-
-    private $plugin;
-
-    public function onEnable()
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        @mkdir($this->getDataFolder());
-        $this->getLogger()->alert(": This Plugin does not manage other plugins loaded through DevTools.");
-    }
-
-    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args)
-    {
-        if(strtolower($cmd->getName())==="manage") {
-            $sender->sendMessage(Color::YELLOW . "Filler Text.");
-            return true;
-        }
-        if(strtolower($cmd->getName())==="enable") {
-            if(count($args) === 1) {
-                if($this->pEnable($args[0])) {
-                    $this->getServer()->broadcastMessage(Color::YELLOW."$args[0] has been Re-Enabled!");
-                    return true;
-                }else{
-                    $sender->sendMessage(Color::YELLOW."$args[0] is already Enabled!");
-                    return true;
-                }
-            }else{
-                $sender->sendMessage(Color::YELLOW . "There needs to be only one argument after this command.");
-                return false;
-            }
-        }
-        if(strtolower($cmd->getName())==="disable") {
-            if(count($args) === 1) {
-                if(!strtolower($args[0])==="plugmanager") {
-                    if($this->pDisable($args[0])){
-                        $this->getServer()->broadcastMessage(Color::YELLOW."$args[0] has been disabled!");
-                        return true;
-                    }else{
-                        $sender->sendMessage(Color::YELLOW."$args[0] is already Disabled!");
-                        return true;
-                    }
-                }else{
-                    $sender->sendMessage(Color::BOLD.Color::RED."You can't disable the Plugin Manager!");
-                }
-                return true;
-            }else{
-                $sender->sendMessage(Color::YELLOW . "There needs to be only one argument after this command.");
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public function pEnable($plugin) {
-        $this->getPluginLoader()->enablePlugin($plugin);
-        return true;
-    }
-
-    public function pDisable($plugin) {
-        $this->getPluginLoader()->disablePlugin($plugin);
-        return true;
-    }
+class PluginManager extends PluginBase {
+	public function onEnable() {
+	 $this->getLogger->notice(TF::GREEN."Enabled");
+		//$this->api->console->register("load", "<plugin name>", array($this, "commandHandler"),15);
+	}
+	public function onCommand(CommandSender $sender, Command $Command, $lists, array $args) {
+	 if(strtolower($command) === "load") {
+	  if(count($args) == 0) {
+	   return false;
+	  }
+	  $plugin = implode(" ",$params);
+				if($this->getServer()->getPluginManager->getPlugin("")){
+					//$this->api->plugin->load(DATA_PATH."plugins/".$plugin.".php");
+					//$info = $this->getPHPPluginInfo(DATA_PATH."plugins/$plugin.php");
+					//$id = $this->api->plugin->getIdentifier($info["name"], $info["author"]);
+					$sender->sendMessage(TF::GREEN."Plugin Enabled!")
+				}else{
+					$sender->sendMessage(TF::RED."Plugin Doesn't exist!");
+				}
+				//$p = $this->api->plugin->get($id);
+				//$p[0]->init();
+				//$output .= "Successfully loaded plugin.";
+			}
+	 }
+	}
+	public function onDisable() {
+	 $this->getLogger()->notice(TF::GREEN."Disabled!");
+	}
 }
